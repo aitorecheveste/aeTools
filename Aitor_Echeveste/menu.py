837db3a64cb41bfa68e3e5a3159441526db92a6d
Aitor Echeveste <72886518+aitorecheveste@users.noreply.github.com>
2021-10-24 21:20:47 +0200

    Update menu.py

--- Aitor_Echeveste/menu.py
+++ Aitor_Echeveste/menu.py
@@ -9,7 +9,6 @@ m.addCommand("aeAnamorphic", "nuke.createNode(\"aeAnamorphic\")", icon="aeAnamor
 m.addCommand("aeFiller", "nuke.createNode(\"aeFiller\")", icon="aeFiller_icon.png")
 m.addCommand("aeBrokenShapes", "nuke.createNode(\"aeBrokenShapes\")", icon="BrokenShapes_icon.png")
 m.addCommand("aePowerPin", "nuke.createNode(\"aePowerPin\")", icon="aePowerPin_icon.png")
-#m.addCommand("aeSnapShot", "nuke.createNode(\"aeSnapShot\")")
 m.addCommand("aeTransform", "nuke.createNode(\"aeTransform\")", icon="aeTransform_icon.png")
 m.addCommand("aeRelight2D", "nuke.createNode(\"aeRelight2D\")", icon="aeReLight2D_icon.png")
 m.addCommand("aeRefracTHOR", "nuke.createNode(\"aeRefracTHOR\")", icon="aeRefracTHOR_icon.png")
