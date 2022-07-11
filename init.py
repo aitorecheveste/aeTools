21eadc5bea162a3f9f9c434d0d08f46d55119434
Aitor Echeveste <72886518+aitorecheveste@users.noreply.github.com>
2022-07-11 12:38:25 +0200

    Add files via upload

--- init.py
+++ init.py
@@ -1,1 +1,18 @@
-nuke.pluginAddPath("./Aitor_Echeveste")
+# python
+import os
+# nuke
+import nuke
+
+
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+nuke.pluginAddPath(os.path.join(dirname, 'Aitor_Echeveste'))
+
+
+
+dirname, filename = os.path.split(os.path.abspath(__file__))
+
+for root, dirs, files in os.walk(dirname):
+    nuke.pluginAddPath(root)
+
+
